0f99a9ca2969c0234cf48533936c5c9450da41ae
abee <yaoxincong@outlook.com>
2020-03-07 12:55:07 +0800

    Add interface demo with python.

--- interface/Python/mailman-video-layer/mailman-video-layer/mailman_video_layer.py
+++ interface/Python/mailman-video-layer/mailman-video-layer/mailman_video_layer.py
@@ -1,8 +1,28 @@
 import requests 
 
+def compose(fps, num_of_pictures, workplace, image_prefix, delete_images):
+    url = ('http://localhost:9123/video/compose?' 
+    + 'fps=' + str(fps) 
+    + '&numOfPictures=' + str(num_of_pictures)
+    + '&workplace=' + workplace
+    + '&imagePrefix=' + image_prefix
+    + '&deleteImages=' + str(delete_images))
 
-url = 'http://localhost:9123/test?parameter=asdasd'
+    request = requests.get(url)
 
-requests = requests.get(url)
+    print (request.content)
 
-print (requests.content)
+def decompose(workplace, image_prefix, video_name):
+    url = ('http://localhost:9123/video/decompose?' 
+    + 'workplace=' + workplace
+    + '&imagePrefix=' + image_prefix
+    + '&videoName=' + video_name)
+
+    request = requests.get(url)
+
+    print (request.content)
+
+
+if __name__ == '__main__':
+    #decompose('D:/Mailman/Workplace', 'decode', 'in.mp4')
+    #compose(5, 19, 'D:/Mailman/Workplace', 'tmp', False)
